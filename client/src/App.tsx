8323b089e97e6782bda4808f3e1309b77245f839
Loïs <131189398+Vonaman@users.noreply.github.com>
2026-03-11 16:26:31 +0100

    fix(package) : Package.json supp de la racine + db move to server (#5)
    * fix(package) : Package.json supp de la racine + db move to server
    
    * fix(env) : Move du .env dans server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   )
